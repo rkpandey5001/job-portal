2bc6b68b2330311313798a7e9a9c0824cabccc84
RAHUL KUMAR PANDEY <rkpandey5001@gmail.com>
2024-10-13 17:59:06 +0530

    first-commit

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -2,6 +2,8 @@ const multer = require("multer");
 
 const storage = multer.memoryStorage();
 
+//add files
+
 const singleUpload = multer({ storage: storage }).single("file");
 
 module.exports = { singleUpload };
